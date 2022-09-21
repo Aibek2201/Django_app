1e15245cbe6c685a127de5eb4a539c7c38bdbbf1
Aibek2201 <a.almabekuly@aues.kz>
2022-09-22 02:14:47 +0600

    Create one app

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t(utsdzw#kscf#jo8$@gt^xxz2llzx)u81u2@kq9w5$6(5wu^@'
+SECRET_KEY = 'django-insecure-#)5=9+s73z$w$pl6o)v7!adpo2uy=cfywa7x$d_)b1y_en(+gy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
