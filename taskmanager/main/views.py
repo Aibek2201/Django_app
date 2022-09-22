3570c74a6e410fef51d8d4e44300391ad829b8fb
Aibek2201 <a.almabekuly@aues.kz>
2022-09-23 00:26:25 +0600

    Completed first project

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
-    return  render(request, "main/index.html")
+    tasks = Task.objects.order_by('-title')
+    return  render(request, "main/index.html", {'title':'Главная страница ','tasks':tasks})
 
 
 def about(request):
-    return  HttpResponse("<h1>About us</h1>")
+    return  render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма была неверной"
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error,
+    }
+
+    return  render(request, 'main/create.html', context)
 
